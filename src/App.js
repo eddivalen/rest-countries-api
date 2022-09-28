b1aaaeafd7c7141ef06d8d4617d9bb7dbb5e62d5
Eddy Valencia <eddyvalencia8@gmail.com>
2022-09-28 15:53:57 -0400

    Added: header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Countries from './components/Countries';
-
+import Header from './components/Header'
 function App() {
   return (
     <>
+      <Header/>
+    <div className="container">
       <Countries />
+    </div>
     </>
   );
 }
