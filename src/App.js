d13207a7a0d9d2aaaf8d121ad25473428acac55f
Eddy Valencia <eddyvalencia8@gmail.com>
2022-10-07 13:05:41 -0400

    Added: Filter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Countries from './components/Countries';
-import Header from './components/Header'
+import Header from './components/Header';
+import Filter from './components/Filter';
 function App() {
   return (
     <>
       <Header/>
     <div className="container">
+      <Filter/>
       <Countries />
     </div>
     </>
