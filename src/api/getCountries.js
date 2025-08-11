652315e46192fee77686c93a2da8e9c76823a03e
Eddy Valencia <eddyvalencia8@gmail.com>
2025-08-11 15:22:20 -0500

    Updated: fetch countries API to include specific fields

--- src/api/getCountries.js
+++ src/api/getCountries.js
@@ -1,6 +1,7 @@
 // fetch countries from API
- const getCountries = async () => {
-  const res = await fetch(process.env.REACT_APP_API_URL+`all`);
+const getCountries = async () => {
+  const fields = 'name,population,region,capital,flags,cca2,cca3,borders,tld,currencies,languages';
+  const res = await fetch(process.env.REACT_APP_API_URL+`all?fields=${fields}`);
   return await res.json();
 }
 
